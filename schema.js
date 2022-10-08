5e41f9f7a1ad46543f74886a155dbeacf3d0cb8f
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-10-08 17:55:04 +0530

    RootQuery implemented

--- schema.js
+++ schema.js
@@ -1,7 +1,10 @@
+const axios = require("axios");
 const {
   GraphQLBoolean,
   GraphQLInt,
+  GraphQLList,
   GraphQLObjectType,
+  GraphQLSchema,
   GraphQLString,
 } = require("graphql");
 
@@ -26,4 +29,49 @@ const RocketType = new GraphQLObjectType({
     rocket_name: { type: GraphQLString },
     rocket_type: { type: GraphQLString },
   }),
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    launches: {
+      type: GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v3/launches")
+          .then((res) => res.data);
+      },
+    },
+    launch: {
+      type: LaunchType,
+      args: { flight_number: { type: GraphQLInt } },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+          .then((res) => res.data);
+      },
+    },
+    rockets: {
+      type: GraphQLList(RocketType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v3/rockets")
+          .then((res) => res.data);
+      },
+    },
+    rocket: {
+      type: RocketType,
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
