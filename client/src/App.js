1c13e5e7b24d9ebd7a0d44fbb5b69d07c8d57eb2
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-10-08 19:28:26 +0530

    Apollo Client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import logo from "./logo.png";
 import "./App.css";
 
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className="App">
-      <img
-        src={logo}
-        alt="SpaceX Logo"
-        style={{ width: 300, display: "block", margin: "auto" }}
-      />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <img
+          src={logo}
+          alt="SpaceX Logo"
+          style={{ width: 300, display: "block", margin: "auto" }}
+        />
+      </div>
+    </ApolloProvider>
   );
 }
 
