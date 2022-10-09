6cef575aaacea301f425de8b39e61d41c13a16dc
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-10-09 16:32:46 +0530

    classnames & moment dependencies installed, launch result expressed by color

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,9 +1,21 @@
+import classNames from "classnames";
+
 function LaunchItem({ launch }) {
   return (
     <div className="card card-body mb-3">
       <div className="row">
         <div className="col-md-9">
-          <h4>Mission: {launch.mission_name}</h4>
+          <h4>
+            Mission:{" "}
+            <span
+              className={classNames({
+                "text-success": launch.launch_success,
+                "text-danger": !launch.launch_success,
+              })}
+            >
+              {launch.mission_name}
+            </span>
+          </h4>
           <p>Date: {launch.launch_date_local}</p>
         </div>
         <div className="col-md-3">
